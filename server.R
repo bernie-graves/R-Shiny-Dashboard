24a49f9a331df9a4db20600f300aaa39884f4df0
laurenrhuang <laurenrhuang@umail.ucsb.edu>
2021-08-09 21:18:44 -0700

    Update ui.R and server.R
    updating the r shinydashboard files with some of the relevant data from movie earnings

--- server.R
+++ server.R
@@ -1,18 +1,15 @@
 library(shinydashboard)
 
 server <- function(input, output) {
-  
+
+# DASHBOARD TAB
+# PLOTS (sample data, haven't changed to csv data yet) 
+
   # sample plot
   set.seed(122)
   histdata <- rnorm(500)
-  
-  # sample slider
-  output$plot1 <- renderPlot({
-    data <- histdata[seq_len(input$slider)]
-    hist(data)
-  })
-  
-  # sample ggplot
+
+  # sample scatterplot
   output$plot2 <- renderPlot({
     plot(mtcars$wt, mtcars$mpg)
   })
@@ -20,4 +17,27 @@ server <- function(input, output) {
   output$info <- renderText({
     paste0("x=", input$plot_click$x, "\ny=", input$plot_click$y)
   })
+  
+  # sample slider
+  output$plot1 <- renderPlot({
+    data <- histdata[seq_len(input$slider)]
+    hist(data)
+  })
+
+  
+# CSV FILE TAB
+
+  # displaying csv file in raw code tab
+  output$contents <- renderTable({
+    read.csv(file = 'Attributes_DataFrame.csv')
+  })
+  
+  # connect csv file and download
+  output$downloadCsv <- downloadHandler(
+    filename = 'Attributes_DataFrame.csv',
+    content = function(file) {
+      write.csv(Attributes_DataFrame, file)
+    }
+  )
+
 }
--- ui.R
+++ ui.R
@@ -1,60 +1,19 @@
+library(shiny)
 library(shinydashboard)
 # check out ggplot2
+library(ggplot2)
+
+# READ DATA
+movie_attributes <- read.csv(file = 'Attributes_DataFrame.csv')
+
 
 dashboardPage(
+  
   skin = "green",
-  dashboardHeader(title = "Basic dashboard",
-                  titleWidth = 350, 
-                  
-        # sample Headers top right corner
-        dropdownMenu(type = "messages",
-                     messageItem(
-                       from = "Sales Dept",
-                       message = "Sales are steady this month."
-                     ),
-                     messageItem(
-                       from = "New User",
-                       message = "How do I register?",
-                       icon = icon("question"),
-                       time = "13:45"
-                     ),
-                     messageItem(
-                       from = "Support",
-                       message = "The new server is ready.",
-                       icon = icon("life-ring"),
-                       time = "2014-12-01"
-                     )),
-        dropdownMenu(type = "notifications",
-                     notificationItem(
-                       text = "5 new users today",
-                       icon("users")
-                     ),
-                     notificationItem(
-                       text = "12 items delivered",
-                       icon("truck"),
-                       status = "success"
-                     ),
-                     notificationItem(
-                       text = "Server load at 86%",
-                       icon = icon("exclamation-triangle"),
-                       status = "warning"
-                     )
-        ),
-         dropdownMenu(type = "tasks", badgeStatus = "success",
-                      taskItem(value = 90, color = "green",
-                               "Documentation"
-                      ),
-                      taskItem(value = 17, color = "aqua",
-                               "Project X"
-                      ),
-                      taskItem(value = 75, color = "yellow",
-                               "Server deployment"
-                      ),
-                      taskItem(value = 80, color = "red",
-                               "Overall project"
-                      )
-         )
-  ),
+  dashboardHeader(title = "Earnings data for movies",
+                  titleWidth = 350
+                  ),
+
   dashboardSidebar(
     width = 350,
     sidebarMenu(
@@ -62,13 +21,8 @@ dashboardPage(
       # Dashboard tab
       menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard")),
       
-      # Widgets tab
-      menuItem("Widgets", icon = icon("th"), tabName = "widgets",
-               badgeLabel = "new", badgeColor = "green"),
-      
-      # Source code tab
-      menuItem("Source code", icon = icon("file-code-o"), 
-               href = "https://github.com/rstudio/shinydashboard/"),
+      # Raw code tab
+      menuItem("Raw code", tabName = "rawcode", icon = icon("file-code-o")),
       
       # Search bar COULD BE HELPFUL!
       sidebarSearchForm(textId = "searchText", buttonId = "searchButton",
@@ -77,73 +31,48 @@ dashboardPage(
     ),
   dashboardBody(
     
-    # Dashboard page content
+# DASHBOARD page content
     tabItems(
       tabItem(tabName = "dashboard",
-              h2("Dashboard tab content"),
+              h2("Movie Earnings analysis"),
               
               # Boxes need to be put in a row (or column)
               fluidRow(
-                
-                # sample Histogram
+                # Histogram
                 box(
                   title = "Histogram", status = "primary", solidHeader = TRUE,
-                  collapsible = TRUE,
-                  plotOutput("plot1", height = 250)
+                  plotOutput("plot1", height = 300)
                 ),
                 
-                # sample ggplot
+                # scatter plot
                 box(
-                  title = "ggplot", status = "primary", solidHeader = TRUE,
-                  collapsible = TRUE,
-                  plotOutput("plot2", click = "plot_click"),
-                  verbatimTextOutput("info")
+                  title = "scatter plot", status = "primary", solidHeader = TRUE,
+                  plotOutput("plot2", height = 300),
                 ),
                 
-                # sample Slider
+                # slider
                 box(
-                  title = "Inputs", status = "warning", solidHeader = TRUE,
-                  "Box content here", br(), "More box content",
-                  sliderInput("slider", "Number of observations:", 1, 100, 50),
+                  title = "Movies", status = "warning", solidHeader = TRUE,
+                  sliderInput("slider", "Runtime in minutes:", 60, 220, 20),
                   textInput("text", "Text input:")
                 ),
                 
-                # sample 2 Tab box
+                # Tab box
                 tabBox(
                   title = "First tabBox",
                   # The id lets us use input$tabset1 on the server to find the current tab
                   id = "tabset1", height = "250px",
                   tabPanel("Tab1", "First tab content"),
                   tabPanel("Tab2", "Tab content 2")
-                ),
-                
-                # sample 3 Tab box, NO Title
-                tabBox(
-                  side = "right", height = "250px",
-                  selected = "Tab3",
-                  tabPanel("Tab1", "Tab content 1"),
-                  tabPanel("Tab2", "Tab content 2"),
-                  tabPanel("Tab3", "Note that when side=right, the tab order is reversed.")
-                )
-              ),
-              fluidRow(
-                
-                # sample Tab STATUS box
-                tabBox(
-                  # Title can include an icon
-                  title = tagList(shiny::icon("gear"), "tabBox status"),
-                  tabPanel("Tab1",
-                           "Currently selected tab from first box:",
-                           verbatimTextOutput("tabset1Selected")
-                  ),
-                  tabPanel("Tab2", "Tab content 2")
                 )
               )
       ),
 
-      # Widgets page content (BLANK FOR NOW)
-      tabItem(tabName = "widgets",
-              h2("Widgets tab content")
+# RAW CODE page content
+      tabItem(tabName = "rawcode",
+              h2("Raw Code"),
+              downloadButton("downloadCsv", "Download as CSV"),
+              tableOutput("contents")
       )
     ),
   ),
